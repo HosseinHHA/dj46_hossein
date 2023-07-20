36c0d7e96efa2ee39ddc84dc9edb4604c61ea86d
HosseinHabibi <hossein_habibi_abyaneh@yahoo.com>
2023-07-20 20:33:17 +0300

    x

---
+++ prim_contact/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+pagina_contacte = """
+<h1>Acesta este un contact fictiv</h1>
+<p>Nume: Hossein </p>
+<p>Pre Nume: Habibi </p>
+<p>Tel: 0123456789 </p>
+"""
